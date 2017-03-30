95826e14540118687f40ca3ba64b0623a1f0f4cf
Frederic <frederic.tausch@student.kit.edu>
2017-03-30 21:24:59 +0200

    Fixed Bugs: 	- regex dot character has not been escaped 	- redirection for autoRedirect was falsly implemented
    Improvents:
    	- Imporved readability by furhter comments

--- ChromePlugin/background.js
+++ ChromePlugin/background.js
@@ -1,4 +1,9 @@
+/*
+ * Add Listener to each webRequest and redirect to smile.amazon.*
+ * Only if settings allow automatic redirection
+ */
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    // Check if automatic redirection is activated
     if (false) {
         var redirects,
             amazonPattern,
@@ -6,10 +11,13 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
             amazon,
             wwwAmazon,
             smile;
-        url = window.location.href;
-        wwwAmazon = "www.amazon.";
-        amazon = "amazon.";
-        smile = "smile.";
+
+        url = details.tab.url;
+        wwwAmazon = "www\\.amazon\\.";
+        amazon = "amazon\\.";
+        smile = "smile\\.";
+
+        // Try to match regex with url
         try {
             amazonPattern = new RegExp(amazon, 'i');
             wwwAmazonPattern = new RegExp(wwwAmazon, 'i');
@@ -18,14 +26,16 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
             matchAmazon = url.match(amazonPattern);
             matchWwwAmazon = url.match(wwwAmazonPattern);
             matchsmile = !url.match(smilePattern);
+
+            // insert smile. infront of amazon.* and remove www.
             if (matchWwwAmazon && matchsmile) {
-                // Redirect
                 var newUrl = url.replace(wwwAmazonPattern, 'smile.amazon.').replace();
-                window.location.href = newUrl;
-            } else if (matchAmazon && matchsmile) {
                 // Redirect
+                return {redirectUrl: newUrl};
+            } else if (matchAmazon && matchsmile) {
                 var newUrl = url.replace(amazonPattern, 'smile.amazon.').replace();
-                window.location.href = newUrl;
+                // Redirect
+                return {redirectUrl: newUrl};
             }
         } catch (err) {
             //bad pattern
@@ -37,21 +47,29 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
     types: ["main_frame"]
 }, ["blocking"]);
 
+/*
+ * Add navigationBar that suggest redirect to each amazon.* page
+ */
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // Check if tab.url is valid
     if (tab.url != null) {
         var redirects,
             amazonPattern,
             smilePattern,
             amazon,
             smile;
-        amazon = 'amazon.';
-        smile = 'smile.amazon.';
+
+        amazon = 'amazon\\.';
+        smile = 'smile\\.amazon.';
+
+        // Try to match regex with url
         try {
             amazonPattern = new RegExp(amazon, 'i');
             smilePattern = new RegExp(smile, 'i');
 
             matchAmazon = tab.url.match(amazonPattern);
             matchsmile = !tab.url.match(smilePattern);
+
             // Matched and not redirect activated, therefore add Navigation Bar
             if (matchAmazon && matchsmile) {
                 chrome.tabs.insertCSS(tabId, {
--- ChromePlugin/contentNavBar.js
+++ ChromePlugin/contentNavBar.js
@@ -1,13 +1,21 @@
+/*
+ * Redirect to about this plugin page at github
+ */
 function aboutSmilesPluginChrome(){
-    var aboutPageUrl = "www.tausch.me/SmileRedirector";
+    var aboutPageUrl = "https://github.com/Langhalsdino/Amazon-Smile-Redirect-Chrome-Extension/tree/master/ChromePlugin";
     window.location.href = aboutPageUrl;
 };
 
+/*
+ * Redirect to smile.amazon.*
+ */
 function redirectMeToSmilesChromePlugin(){
     var url = window.location.href;
-    var wwwAmazon = "www.amazon.";
-    var amazon = "amazon.";
-    var smile = "smile.";
+    var wwwAmazon = "www\\.amazon\\.";
+    var amazon = "amazon\\.";
+    var smile = "smile\\.";
+
+    // Try to match regex with url
     try {
         amazonPattern = new RegExp(amazon, 'i');
         wwwAmazonPattern = new RegExp(wwwAmazon, 'i');
@@ -16,13 +24,15 @@ function redirectMeToSmilesChromePlugin(){
         matchAmazon = url.match(amazonPattern);
         matchWwwAmazon = url.match(wwwAmazonPattern);
         matchsmile = !url.match(smilePattern);
+
+        // insert smile. infront of amazon.* and remove www.
         if (matchWwwAmazon && matchsmile) {
-            // Redirect
             var newUrl = url.replace(wwwAmazonPattern, 'smile.amazon.').replace();
+            // Redirect
             window.location.href = newUrl;
         } else if (matchAmazon && matchsmile) {
-            // Redirect
             var newUrl = url.replace(amazonPattern, 'smile.amazon.').replace();
+            // Redirect
             window.location.href = newUrl;
         }
     } catch (err) {
@@ -31,7 +41,9 @@ function redirectMeToSmilesChromePlugin(){
 }
 
 
-// Only add one navBar per Page
+/*
+ * Add NavBar to Page, but only add one navBar per page
+ */
 if(document.getElementById('smileRedirectExtension') === null){
     var navBar = "<ul id='smileRedirectExtension'>\
                     <div class='floatLeft'>\
@@ -49,6 +61,8 @@ if(document.getElementById('smileRedirectExtension') === null){
                     </div>\
                 </ul>";
     document.body.innerHTML = navBar + document.body.innerHTML;
+
+    // Add click functions to Buttons
     document.getElementById('redirectToSmilesLiItem').addEventListener("click", redirectMeToSmilesChromePlugin);
     document.getElementById('aboutSmilesPluginLiItem').addEventListener("click", aboutSmilesPluginChrome);
 
